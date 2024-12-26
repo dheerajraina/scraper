29e80b1cd872ff3d7e0df5897929e50610bac63e
Dheeraj Raina <dheerajraina55@gmail.com>
2024-12-26 15:13:55 +0530

    notification testing

---
+++ notifier.py
@@ -0,0 +1,7 @@
+from plyer import notification
+
+notification.notify(
+                title="New update",
+                message="The result has been updated",
+                timeout=10
+        )
